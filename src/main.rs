67f83792a972f48de4053edda0a766270fd25651
carrotflakes <carrotflakes@gmail.com>
2023-02-13 02:59:30 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,151 @@
-use exprgen::instruction::Inst;
-use exprgen::instruction::Instruction;
-use exprgen::run;
+#[derive(Debug)]
+pub enum Src {
+    Const(i32),
+    Var(usize),
+}
 
-pub fn generate(len: usize, mut stack_size: usize, mut seed: u128) -> Vec<Inst> {
-    let mut cs = Vec::new();
-    while cs.len() < len {
-        if len - cs.len() < stack_size {
-            let n = 4;
-            let inst = match seed % n {
-                0 => Inst::Add,
-                1 => Inst::Sub,
-                2 => Inst::Mul,
-                3 => Inst::Mod,
-                4.. => unreachable!(),
-            };
-            stack_size += inst.arity().1;
-            stack_size -= inst.arity().0;
-            cs.push(inst);
-            seed = seed / n;
+#[derive(Debug)]
+pub struct NormalForm {
+    src: Src,
+    abs: bool,
+    negative: bool,
+    add: Option<Box<NormalForm>>,
+    mul: Option<Box<NormalForm>>,
+    pow: Option<Box<NormalForm>>,
+    div: Option<Box<NormalForm>>,
+    rem: Option<Box<NormalForm>>,
+}
 
-            continue;
-        }
-        // if len == cs.len() + 1 && stack_size == 1 {
-        //     let n = 1;
-        //     let inst = match seed % n {
-        //         0 => Command::Neg,
-        //         1.. => unreachable!(),
-        //     };
-        //     stack_size += inst.arity().1;
-        //     stack_size -= inst.arity().0;
-        //     cs.push(inst);
-        //     seed = seed / n;
-        // }
-
-        let mut n = 6;
-        if 1 <= stack_size {
-            n += 2;
-        }
-        if 2 <= stack_size {
-            n += 5;
-        }
-        if 3 <= stack_size {
-            n += 1;
-        }
-        let inst = match seed % n {
-            i @ (0..=5) => Inst::PushConst([0, 1, 2, 3, 5, 7][i as usize]),
-            6 => Inst::Dup,
-            7 => Inst::Neg,
-            8 => Inst::Add,
-            9 => Inst::Sub,
-            10 => Inst::Mul,
-            11 => Inst::Mod,
-            12 => Inst::Swap,
-            13 => Inst::Rot,
-            14.. => unreachable!(),
+impl NormalForm {
+    pub fn format(&self) -> String {
+        let mut str = match self.src {
+            Src::Const(v) => v.to_string(),
+            Src::Var(i) => format!("v{}", i),
         };
-        stack_size += inst.arity().1;
-        stack_size -= inst.arity().0;
-        cs.push(inst);
-        seed = seed / n;
+        if self.abs {
+            str = format!("abs({})", str);
+        }
+        if self.negative {
+            str = format!("-{}", str);
+        }
+        if let Some(add) = &self.add {
+            str = format!("({}+({}))", str, add.format());
+        }
+        if let Some(mul) = &self.mul {
+            str = format!("({}*({}))", str, mul.format());
+        }
+        if let Some(pow) = &self.pow {
+            str = format!("{}^({})", str, pow.format());
+        }
+        if let Some(div) = &self.div {
+            str = format!("{}/({})", str, div.format());
+        }
+        if let Some(rem) = &self.rem {
+            str = format!("{}%({})", str, rem.format());
+        }
+        str
     }
-    cs
+
+    pub fn compute(&self, vars: &[i32]) -> i32 {
+        let mut v = match &self.src {
+            Src::Const(v) => *v,
+            Src::Var(i) => vars[*i],
+        };
+        if self.abs {
+            v = v.abs();
+        }
+        if self.negative {
+            v = -v;
+        }
+        if let Some(add) = &self.add {
+            v = v.overflowing_add(add.compute(vars)).0;
+        }
+        if let Some(mul) = &self.mul {
+            v = v.overflowing_mul(mul.compute(vars)).0;
+        }
+        if let Some(pow) = &self.pow {
+            v = v.overflowing_pow(pow.compute(vars) as u32).0;
+        }
+        if let Some(div) = &self.div {
+            let x = div.compute(vars);
+            if x == 0 {
+                v = 0;
+            } else {
+                v /= x;
+            }
+        }
+        if let Some(rem) = &self.rem {
+            let x = rem.compute(vars);
+            if x == 0 {
+                v = 0;
+            } else {
+                v %= x;
+            }
+        }
+        v
+    }
+}
+
+pub fn generate(seed: u128) -> NormalForm {
+    let v = (seed % 8) as i32;
+    NormalForm {
+        src: if v == 7 { Src::Var(0) } else { Src::Const(v) },
+        abs: (seed >> 4) & 1 == 1,
+        negative: (seed >> 5) & 1 == 1,
+        add: if (seed >> 6) & 1 == 1 {
+            Some(Box::new(generate((seed >> 11) + 1)))
+        } else {
+            None
+        },
+        mul: if (seed >> 7) & 1 == 1 {
+            Some(Box::new(generate((seed >> 12) + 1)))
+        } else {
+            None
+        },
+        pow: if (seed >> 8) & 1 == 1 {
+            Some(Box::new(generate((seed >> 13) + 1)))
+        } else {
+            None
+        },
+        div: if (seed >> 9) & 1 == 1 {
+            Some(Box::new(generate((seed >> 14) + 1)))
+        } else {
+            None
+        },
+        rem: if (seed >> 10) & 1 == 1 {
+            Some(Box::new(generate((seed >> 15) + 1)))
+        } else {
+            None
+        },
+    }
 }
 
 fn main() {
-    use Inst::*;
-    dbg!(run(vec![], &[PushConst(1), PushConst(2), Add, Dup, Mul]));
+    let form = NormalForm {
+        src: Src::Const(0),
+        abs: false,
+        negative: false,
+        add: None,
+        mul: None,
+        pow: None,
+        div: None,
+        rem: None,
+    };
+    dbg!(form.compute(&[]));
 
-    println!("{:?}", generate(3, 0, 123450));
-    println!("{:?}", generate(4, 0, 1234567));
-    println!("{:?}", run(vec![], &generate(4, 0, 1234567)));
-    println!("{:?}", generate(5, 0, 12345678));
-    println!("{:?}", run(vec![], &generate(5, 0, 12345678)));
-    println!("{:?}", generate(6, 0, 123456789));
-    println!("{:?}", run(vec![], &generate(6, 0, 123456789)));
-
-    println!();
-
+    dbg!(generate(0));
+    dbg!(generate(1));
+    dbg!(generate(2));
+    dbg!(generate(3));
+    dbg!(generate(4));
+    dbg!(generate(1234567).format());
+    
     for i in 0..10 {
-        let code = generate(6, 1, 123456789 + i * 123);
-        println!("{:?}", &code);
+        let form = generate(1234567 + i * 123);
+        println!("{:?}", &form.format());
         for j in 0..20 {
-            let stack = run(vec![j as i32], &code);
-            let res = stack.last().unwrap();
-            print!("{},", res);
+            let v = form.compute(&[j as i32]);
+            print!("{}, ", v);
         }
         println!();
     }
